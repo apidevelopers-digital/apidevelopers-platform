7e87e3a459e82dfbb88f9606783c4fba46af0fff
sitedauni <abreuigor653@gmail.com>
2026-07-20 19:42:17 -0300

    feat(onboarding-core): export public contract

---
+++ packages/onboarding-core/src/index.mjs
@@ -0,0 +1,4 @@
+export { OnboardingDomainError } from "./common.mjs";
+export { ONBOARDING_STATES, ONBOARDING_STEPS, createOnboarding } from "./model.mjs";
+export { InMemoryOnboardingRepository } from "./repository.mjs";
+export { OnboardingService } from "./service.mjs";
